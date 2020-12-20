c4c6a7d83421b0a0782fdd3e6c419f5165c62cb9
Luzanne Weda <luzanne.weda@gmail.com>
2020-12-20 16:56:52 +0100

    Day 20 Refactored ugly code

--- 2020/day20/index.js
+++ 2020/day20/index.js
@@ -6,15 +6,16 @@ const seamonster = [
   " #  #  #  #  #  #   ".split('')
 ]
 
+const rotate = array => array[0].map((_, colIndex) => array.map(row => row[colIndex]).reverse())
+const transpose = array => array[0].map((_, colIndex) => array.map(row => row[colIndex]))
+const reverseString = string => string.split('').reverse().join('')
+
 class Tile {
-  constructor() {
-    this.id = null
+  constructor(id) {
+    this.id = id
     this.tiles = []
     this.matchingTiles = []
     this.fitted = false
-  }
-  setId(id) {
-    this.id = id
   }
 
   addTileLine(line) {
@@ -33,24 +34,18 @@ class Tile {
     this.flippedleft = `${this.top}`
 
     this.edges = [
-      this.top,
-      this.right,
-      this.bottom,
-      this.left,
-      [...this.top].reverse().join(''),
-      [...this.right].reverse().join(''),
-      [...this.bottom].reverse().join(''),
-      [...this.left].reverse().join('')
+      this.top, this.right, this.bottom, this.left,
+      reverseString(`${this.top}`), reverseString(`${this.right}`), reverseString(`${this.bottom}`), reverseString(`${this.left}`)
     ]
   }
 
   rotate () {
-    this.tiles = this.tiles[0].map((_, colIndex) => this.tiles.map(row => row[colIndex]).reverse())
+    this.tiles = rotate(this.tiles)
     this.generateEdges()
   }
 
   flip () {
-    this.tiles = this.tiles[0].map((_, colIndex) => this.tiles.map(row => row[colIndex]))
+    this.tiles = transpose(this.tiles)
     this.generateEdges()
   }
 
@@ -64,63 +59,26 @@ class Tile {
   }
 
   fit (otherTile) {
+    const processMatch = (direction, otherDirection) => {
+      if (this[direction] === otherTile[`flipped${otherDirection}`]) {
+        otherTile.flip()
+        while (this[direction] !== otherTile[otherDirection]) {
+          otherTile.rotate()
+        }
+      } else if (this[direction] !== otherTile[otherDirection]) {
+        return false
+      }
+
+      this[`matched${direction}tile`] = otherTile.id
+      otherTile[`matched${otherDirection}tile`] = this.id
+      return true
+    }
     for (let i = 0; i < 4; i++) {
-      if (this.top === otherTile.bottom) {
-        this.matchedtoptile = otherTile.id
-        otherTile.matchedbottomtile = this.id
-        this.fitted = true
-        return
-      } else if (this.top === otherTile.flippedbottom) {
-        otherTile.flip()
-        while (this.top !== otherTile.bottom) {
-          otherTile.rotate()
-        }
-        this.matchedtoptile = otherTile.id
-        otherTile.matchedbottomtile = this.id
-        this.fitted = true
-        return
-      } else if (this.right === otherTile.left) {
-        this.matchedrighttile = otherTile.id
-        otherTile.matchedlefttile = this.id
-        this.fitted = true
-        return
-      } else if (this.right === otherTile.flippedleft) {
-        otherTile.flip()
-        while(this.right !== otherTile.left) {
-          otherTile.rotate()
-        }
-        this.matchedrighttile = otherTile.id
-        otherTile.matchedlefttile = this.id
-        this.fitted = true
-        return
-      } else if (this.bottom === otherTile.top) {
-        this.matchedbottomtile = otherTile.id
-        otherTile.matchedtoptile = this.id
-        this.fitted = true
-        return
-      } else if (this.bottom === otherTile.flippedtop) {
-        otherTile.flip()
-        while(this.bottom !== otherTile.top) {
-          otherTile.rotate()
-        }
-        this.matchedbottomtile = otherTile.id
-        otherTile.matchedtoptile = this.id
-        this.fitted = true
-        return
-      } else if (this.left === otherTile.right) {
-        this.matchedlefttile = otherTile.id
-        otherTile.matchedrighttile = this.id
-        this.fitted = true
-        return
-      } else if (this.left === otherTile.flippedright) {
-        otherTile.flip()
-        while(this.left !== otherTile.right) {
-          otherTile.rotate()
-        }
-        this.matchedlefttile = otherTile.id
-        otherTile.matchedrighttile = this.id
-        this.fitted = true
-        return
+      if (processMatch('top', 'bottom') || 
+          processMatch('right', 'left') ||
+          processMatch('bottom', 'top') || 
+          processMatch('left', 'right')) {
+        break;
       }
 
       otherTile.rotate()
@@ -132,14 +90,13 @@ async function processLineByLine() {
   let input = await readFile('2020/day20/input.txt')
 
   const images = []
-  let currentTile = new Tile()
+  let currentTile = null
   input.forEach(line => {
     if (line.startsWith('Tile ')) {
-      currentTile.setId(Number(line.substring(5, 9)))
+      currentTile = new Tile(Number(line.substring(5, 9)))
     } else if (line === '') {
       currentTile.generateEdges()
       images.push(currentTile)
-      currentTile = new Tile()
     } else {
       currentTile.addTileLine(line)
     }
@@ -148,18 +105,18 @@ async function processLineByLine() {
   currentTile.generateEdges()
   images.push(currentTile)
 
+  // Prepare list with adjacent tiles
+  images.forEach(tile => {
+    const otherTiles = images.filter(x => x.id !== tile.id)
+    otherTiles.forEach(otherTile => tile.adjacentTile(otherTile))
+    console.log('Tile ' + tile.id + ' found ' + tile.matchingTiles.length + ' adjacent tiles')
+  })
+
   puzzle1(images)
   puzzle2(images)
 }
 
 function puzzle1(input) {
-  input.forEach(tile => {
-    // console.log('Check tile ' + tile.id)
-    const otherTiles = input.filter(x => x.id !== tile.id)
-    otherTiles.forEach(otherTile => tile.adjacentTile(otherTile))
-    console.log('Tile ' + tile.id + ' found ' + tile.matchingTiles.length + ' adjacent tiles')
-  })
-
   const corners = input.filter(tile => tile.matchingTiles.length === 2)
   console.log(`Puzzle 1: ${corners[0].id * corners[1].id * corners[2].id * corners[3].id}`)
 }
@@ -175,7 +132,6 @@ function findSeamonster(image, x, y, replace = false) {
           if (replace) {
             image[y+sy][x+sx] = 'X'
           }
-          continue
         }
         else {
           foundSeaMonster = false
@@ -190,9 +146,6 @@ function findSeamonster(image, x, y, replace = false) {
 
   return foundSeaMonster
 }
-
-const rotate = (array) => array[0].map((_, colIndex) => array.map(row => row[colIndex]).reverse())
-const transpose = (array) => array[0].map((_, colIndex) => array.map(row => row[colIndex]))
 
 function puzzle2(input) {
   const corners = input.filter(tile => tile.matchingTiles.length === 2)
@@ -232,18 +185,15 @@ function puzzle2(input) {
   }
   
   let actualImage = []
-  // Remove borders from image + log image to debug
+  // Remove borders from image
   totalimage.forEach(row => {
     for (let x = 1; x < row[0].length - 1; x++) {
-      let rowStr = []
+      let rowArray = []
       for (let y = 0; y < row.length; y++) {
-        rowStr.push(row[y][x].join('').substring(1, 9))
+        rowArray.push(row[y][x].join('').substring(1, 9))
       }
-
-      actualImage.push(rowStr.join('').split(''))
-      // console.log(rowStr.join(' '))
+      actualImage.push(rowArray.join('').split(''))
     }
-    // console.log('')
   })
 
   let seaMonsterLocations = []
@@ -253,7 +203,6 @@ function puzzle2(input) {
     for (let y = 0; y < actualImage.length - seamonster.length; y++) {
       for (let x = 0; x < actualImage[y].length - seamonster[0].length; x++) {
         let foundSeaMonster = findSeamonster(actualImage, x, y, false)
-
         if (foundSeaMonster) {
           seaMonsterLocations.push([x, y])
         }
